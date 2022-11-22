fede7e3b565c4c46ee9e6bac52ccff556331e3cb
pravekum55 <praveenkumar0809.sm@gmail.com>
2022-11-22 23:14:18 +0530

    Add files via upload

---
+++ my-weather-new-app/src/api.js
@@ -0,0 +1,14 @@
+export const geoApiOptions = {
+    method: 'GET',
+   
+};
+
+export const GEO_API_URL = "http://api.weatherapi.com/v1/search.json";
+
+export const CURRENT_API_URL = "http://api.weatherapi.com/v1/current.json";
+
+export const FORECAST_API_URL = "http://api.weatherapi.com/v1/forecast.json";
+
+export const WEATHER_API_KEY = "<API-KEY";
+
+
